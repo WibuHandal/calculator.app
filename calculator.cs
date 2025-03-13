7c24e6d5b22769a1b1face792f14b1e6544682b0
Aryaneta <blewah092@gmail.com>
2025-03-13 11:31:41 +0700

    cek projek

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,22 @@ class calculator
 {
 static void Main(string[] args)
 {
+int a = 10;
+int b = 6;
 
+Console.Writeline("Hasil penambahan:{0} + {1} = {2}",a,b, penambahan(a,b));
+Console.Writeline("Hasil penambahan:{0} - {1} = {2}",a,b, pengurangan(a,b));
+
+Console.Writeline("\nTekan sembarang untuk keluar");
+Console.Readkey();
 }
 
+static int Penambahan(int a, int b)
+{
+return a + b;	
+}
+static int Pengurangan(int a , int b)
+{
+	return a - b;
+}
 }
